5731fcdbc1d39171717c1f0eb5da2f49e01059dd
Hideto Maguchi <hidetomaguchi@gmail.com>
2024-09-19 22:31:41 +0900

    test & refuctoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,11 +29,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
-      render :new, status: :unprocessable_entity # 422 Unprocessable Entity 
+      render 'new', status: :unprocessable_entity
     end
   end
 
