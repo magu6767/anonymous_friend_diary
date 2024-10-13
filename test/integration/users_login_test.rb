e9a1ee4f2a5184ffc4a11b708e5a1bd41a38ef0d
Hideto Maguchi <hidetomaguchi@gmail.com>
2024-10-13 17:51:13 +0900

    logout_pathの書き方を変更

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -45,7 +45,6 @@ class ValidLoginTest < ValidLogin
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
   end
 end
