402b1c5378fc62c69fc5f9b64ce87c2412110f02
Hideto Maguchi <hidetomaguchi@gmail.com>
2024-10-14 23:59:24 +0900

    bootstrapの際導入

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem "puma",            "5.6.8"
 gem "bootsnap",        "1.16.0", require: false
 gem "sqlite3",         "1.6.1"
 gem "sassc-rails", ">= 2.1.0"
+gem "bootstrap", "~> 5.3.0"
+gem "jquery-rails"
 
 group :development, :test do
   gem "debug",   "1.7.1", platforms: %i[ mri mingw x64_mingw ]
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,10 @@
+// この３つのrequireの意味は分かってない
+//= require jquery3
+//= require popper
+//= require bootstrap
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
-import { Tooltip, Popover } from "bootstrap"
+
 
