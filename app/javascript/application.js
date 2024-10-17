ede9dbbcf085e956ba317da38027e1229829f32c
Hideto Maguchi <hidetomaguchi@gmail.com>
2024-10-17 23:59:22 +0900

    bootstrapの導入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,3 @@
-// この３つのrequireの意味は分かってない
-//= require jquery3
-//= require popper
-//= require bootstrap
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// app/javascript/application.js
 import "bootstrap"
-
-
+import "@popperjs/core"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,5 @@
 # Pin npm packages by running ./bin/importmap
-
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap.min.js"
-pin "@popperjs/core", to: "popper.js"
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js"
+
