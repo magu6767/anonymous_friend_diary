38908e38d4aab66ab290d11be35690d490355e8e
Hideto Maguchi <hidetomaguchi@gmail.com>
2024-11-03 22:45:49 +0900

    静的ファイルを提供するよう設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,7 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-  config.assets.compile = false
+  config.public_file_server.enabled = true
   config.active_storage.service = :local
   config.force_ssl = true
   config.log_level = :info
