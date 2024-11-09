e37a3611777c789fd0a81ed40f46cf4a65bb6d21
Hideto Maguchi <hidetomaguchi@gmail.com>
2024-11-09 20:16:42 +0900

    ログイン前後の表示切り替え

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -6,22 +6,17 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:one)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
+    assert_equal "アカウント有効化のお知らせ", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["account-verification@anonymous-friend-diary.com"], mail.from
-    assert_match user.name,               mail.body.encoded
-    assert_match user.activation_token,   mail.body.encoded
-    assert_match CGI.escape(user.email),  mail.body.encoded
   end
 
   test "password_reset" do
     user = users(:one)
     user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
-    assert_equal "Password reset", mail.subject
+    assert_equal "パスワードのリセット", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["account-verification@anonymous-friend-diary.com"], mail.from
-    assert_match user.reset_token,        mail.body.encoded
-    assert_match CGI.escape(user.email),  mail.body.encoded
   end
 end
