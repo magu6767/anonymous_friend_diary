f12d14371b885b2966bd23651c1955c7b22c3f5b
Hideto Maguchi <hidetomaguchi@gmail.com>
2024-11-23 16:30:25 +0900

    コメント機能の追加1

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,5 @@ class Post < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :title, presence: true
   validates :content, presence: true, length: { maximum: 20000 }
+  scope :with_comments, -> { joins(:comments).distinct }
 end
