30d45a83944e7a672f88e60d062d16b65e128182
Hideto Maguchi <hidetomaguchi@gmail.com>
2025-01-21 18:01:58 +0900

    Remove the with_comments scope from the Post model to eliminate comment-related functionality.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,5 +6,4 @@ class Post < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :title, presence: true
   validates :content, presence: true, length: { maximum: 20000 }
-  scope :with_comments, -> { joins(:comments).distinct }
 end
